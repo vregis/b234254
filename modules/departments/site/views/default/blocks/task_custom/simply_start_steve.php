b07d2c52ceadf541fc953ca70255cb25df8bc1dd
Александр Смирнов <darth.smirnov@yandex.ru>
2016-02-27 00:12:26 +0300

    fixes

--- modules/departments/site/views/default/blocks/task_custom/simply_start_steve.php
+++ modules/departments/site/views/default/blocks/task_custom/simply_start_steve.php
@@ -20,7 +20,7 @@ use modules\tasks\models\Task;
                             <label for="Roadmap[<?= $i ?>]">
                                 <span></span>
                                 <span class="check"></span>
-                                <span class="box" style="cursor: default" onclick="return false;">1</span>
+                                <span class="box" style="cursor: default" onclick="return false;"><?=$i==0 ? '<i class="fa fa-check font-green-jungle"></i>' : $i + 1?></span>
                             </label>
                             <div class="text-desc-task" style="display: none">
                                 <?= $task->description ?>
