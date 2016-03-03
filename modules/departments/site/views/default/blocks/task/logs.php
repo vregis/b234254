a3415e0c66a11af9f86107a54c3c57005d40cf57
Александр Смирнов <darth.smirnov@yandex.ru>
2016-03-03 03:07:34 +0300

    fixes

--- modules/departments/site/views/default/blocks/task/logs.php
+++ modules/departments/site/views/default/blocks/task/logs.php
@@ -1,6 +1,6 @@
 <? foreach($taskUserLogs as $taskUserLog) : ?>
     <? $oDate = new DateTime($taskUserLog->date); ?>
-    <li><?= $oDate->format("m/d/Y") ?> <br>
-        <?= $taskUserLog->log ?>
+    <li style="font-size:14px;"><span><?= $oDate->format("m/d/Y") ?></span> <br>
+        <span style="font-size:12px;"><strong><?= $taskUserLog->log ?></strong></span>
     </li>
 <? endforeach; ?>
